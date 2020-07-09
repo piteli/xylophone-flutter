9d27cc3233ef6773c4c90ad869da89842d0443d3
piteli <rushwender@gmail.com>
2020-07-09 09:58:10 +0800

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -10,54 +10,33 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$number.wav');
   }
 
+  Widget buildLayout(Color color, int key){
+    return Expanded(
+        child : FlatButton(
+            color: color,
+            onPressed: (){
+              playSound(key);
+            }, child: Text('Click Me')
+        )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Column(children : <Widget>[
-            FlatButton(
-              color: Colors.red,
-                onPressed: (){
-                  playSound(1);
-            }, child: Text('Click Me')
-            ),
-            FlatButton(
-                color: Colors.orange,
-                onPressed: (){
-                  playSound(2);
-                }, child: Text('Click Me')
-            ),
-            FlatButton(
-                color: Colors.yellow,
-                onPressed: (){
-                  playSound(3);
-                }, child: Text('Click Me')
-            ),
-            FlatButton(
-                color: Colors.green,
-                onPressed: (){
-                  playSound(4);
-                }, child: Text('Click Me')
-            ),
-            FlatButton(
-                color: Colors.teal,
-                onPressed: (){
-                  playSound(5);
-                }, child: Text('Click Me')
-            ),
-            FlatButton(
-                color: Colors.blue,
-                onPressed: (){
-                  playSound(6);
-                }, child: Text('Click Me')
-            ),
-            FlatButton(
-                color: Colors.purple,
-                onPressed: (){
-                  playSound(7);
-                }, child: Text('Click Me')
-            ),
+          backgroundColor : Colors.black,
+          body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+              children : <Widget>[
+                buildLayout(Colors.red, 1),
+                buildLayout(Colors.orange, 2),
+                buildLayout(Colors.yellow, 3),
+                buildLayout(Colors.green, 4),
+                buildLayout(Colors.teal, 5),
+                buildLayout(Colors.blue, 6),
+                buildLayout(Colors.purple, 7)
 
           ]),
         ),
